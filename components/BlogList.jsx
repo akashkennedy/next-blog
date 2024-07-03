bc420d57a945736501c0db9f4d374fa3d6802fe0
Akash Kennedy <akashkennedy1@gmail.com>
2024-07-03 06:02:37 +0530

    Create Filter Feature to Blogs

--- components/BlogList.jsx
+++ components/BlogList.jsx
@@ -1,29 +1,64 @@
 import { blog_data } from "@/Public/assets";
 import BlogItem from "./BlogItem";
+import { useState } from "react";
 
 const BlogList = () => {
+  const [menu, setMenu] = useState("All");
+
   return (
     <div>
       {" "}
       <div className="flex justify-center gap-6 my-10">
-        <button className="bg-black text-white py-1 px-4 rounded-sm">
+        <button
+          className={
+            menu === "All" ? "bg-black text-white py-1 px-4 rounded-sm" : ""
+          }
+          onClick={() => setMenu("All")}
+        >
           All
         </button>
-        <button>Technology</button>
-        <button>Startup</button>
-        <button>Lifestyle</button>
+        <button
+          onClick={() => setMenu("Technology")}
+          className={
+            menu === "Technology"
+              ? "bg-black text-white py-1 px-4 rounded-sm"
+              : ""
+          }
+        >
+          Technology
+        </button>
+        <button
+          onClick={() => setMenu("Startup")}
+          className={
+            menu === "Startup" ? "bg-black text-white py-1 px-4 rounded-sm" : ""
+          }
+        >
+          Startup
+        </button>
+        <button
+          onClick={() => setMenu("Lifestyle")}
+          className={
+            menu === "Lifestyle"
+              ? "bg-black text-white py-1 px-4 rounded-sm"
+              : ""
+          }
+        >
+          Lifestyle
+        </button>
       </div>
       <div className="flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24">
-        {blog_data.map((item, index) => {
-          return (
-            <BlogItem
-              image={item.image}
-              description={item.description}
-              title={item.title}
-              key={index}
-            />
-          );
-        })}
+        {blog_data
+          .filter((item) => (menu === "All" ? true : item.category === menu))
+          .map((item, index) => {
+            return (
+              <BlogItem
+                image={item.image}
+                description={item.description}
+                title={item.title}
+                key={index}
+              />
+            );
+          })}
       </div>
     </div>
   );
