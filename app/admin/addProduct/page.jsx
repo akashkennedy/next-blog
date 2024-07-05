2bf8ef3b59c22bfca86083ec019b53e5b11c1231
Akash Kennedy <akashkennedy1@gmail.com>
2024-07-05 21:04:49 +0530

    Created Admin Panel UI

--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -1,8 +1,64 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
+import Image from "next/image";
+import { assets } from "@/Public/assets";
 
 const page = () => {
-  return <div>page</div>;
+  const [image, setImage] = useState(false);
+
+  return (
+    <>
+      <form action="" className="pt-5 px-5 sm:pt-12 sm:pl-16">
+        <p className="text-xl">Upload Thumbnail</p>
+        <label htmlFor="image">
+          <Image
+            className="mt-4 cursor-pointer"
+            src={!image ? assets.upload_area : URL.createObjectURL(image)}
+            width={140}
+            height={70}
+            alt=""
+          />
+        </label>
+        <input
+          type="file"
+          id="image"
+          hidden
+          required
+          onChange={(e) => setImage(e.target.files[0])}
+        />
+        <p className="text-xl mt-4">Blog Title</p>
+        <input
+          type="text"
+          placeholder="Type Here..."
+          required
+          className="w-full sm:w-[500px] mt-4 px-4 py-3 border"
+        />
+        <p className="text-xl mt-4">Blog Description</p>
+        <textarea
+          type="text"
+          placeholder="Write Content Here..."
+          required
+          className="w-full sm:w-[500px] mt-4 px-4 py-3 border resize-none"
+          rows={6}
+        />
+        <p className="text-xl mt-4">Blog Category</p>
+        <select
+          name="category"
+          className="w-40 mt-4 px-4 py-3 border text-gray-500"
+        >
+          <option value="Startup">Startup</option>
+          <option value="Technology">Technology</option>
+          <option value="Lifestyle">Lifestyle</option>
+        </select>
+        <button
+          type="submit"
+          className="my-3 w-40 h-10 bg-black text-white block"
+        >
+          Add
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default page;
